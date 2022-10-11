60cc9d655afffb7b251ac21a31e3354769db8725
Robert Hoffmann <robert.hoffmann@gematik.de>
2022-10-11 12:02:47 +0200

    moved clear statement

--- validate.sh
+++ validate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 # This script will download the latest validation script from the template source save it
 # to the scripts_folder and execute it to validate your fhir ressources.
 script_name=./scripts/validate_script.sh
@@ -6,6 +7,6 @@ script_name=./scripts/validate_script.sh
 parentdir="$(dirname "$script_name")"
 mkdir -p "$parentdir"
 
-curl https://raw.githubusercontent.com/gematik/spec-TemplateForSimplifierProjects/scripts/scripts/validate.sh -o $script_name
+curl https://raw.githubusercontent.com/gematik/spec-TemplateForSimplifierProjects/master/scripts/validate_script.sh -o $script_name
 chmod a+x $script_name
 $script_name
