ef65fd1cfdccb5cfb289ec126073e540d714dcbb
Florian Schoffke <florian.schoffke@gematik.de>
2022-10-12 09:53:23 +0200

    Adjust location references for setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm ../Resources/input/fsh/*
-rm ../Resources/fsh-generated/resources/*
-rm ../scripts/downloaded_validation_script.sh
-rm ../scripts/install_fhir_tools.sh
-rm ../README.md
-mv ../Readme_template ./README.md
-rm ../scripts/setup.sh
+rm ./Resources/input/fsh/*
+rm ./Resources/fsh-generated/resources/*
+rm ./scripts/downloaded_validation_script.sh
+rm ./scripts/install_fhir_tools.sh
+rm ./README.md
+mv ./Readme_template ./README.md
+rm ./scripts/setup.sh
