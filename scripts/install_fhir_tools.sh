205458c4b19376300eeea84b293685c8f4fa31c6
Robert Hoffmann <klaas.klaar@googlemail.com>
2022-10-20 13:32:06 +0200

    Update install_fhir_tools.sh
    fixed order for installing firely.terminal

--- scripts/install_fhir_tools.sh
+++ scripts/install_fhir_tools.sh
@@ -4,7 +4,7 @@ sudo apt install openjdk-17-jre-headless -y
 
 echo "##### Installing Sushi"
 sudo npm install -g fsh-sushi
-sudo dotnet tool install --global firely.terminal --version 2.5.0-beta-7
+
 echo "##### Installing dotnet-sdk-3.1"
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -15,7 +15,7 @@ sudo apt-get update; \
   sudo apt-get install -y dotnet-sdk-3.1
 
 echo "##### Installing firely.terminal"
-
+sudo dotnet tool install --global firely.terminal --version 2.5.0-beta-7
 
 echo "##### Installing yq yaml-parser"
 sudo snap install yq
