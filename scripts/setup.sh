7188325b3bc9dd723e50f5b1ca363a8981d24107
developer <dev@FHIR.mshome.net>
2022-11-02 20:10:58 +0100

    review

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,7 @@ rm ./Resources/input/fsh/*
 rm ./Resources/fsh-generated/resources/*
 rm ./README.md
 cat Readme_template.md USAGE.md > ./README.md
+rm Readme_template.md
 rm USAGE.md
 rm INSTALL.md
 rm ./scripts/setup.sh
