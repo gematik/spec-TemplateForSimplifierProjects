4b9ccc8f9bdf3bcc89147b6db6031e805e6ad891
Francois Peverali <francois-peverali@gematik.de>
2023-03-09 17:02:44 +0100

    add: replace version in Einfuehrung.md

--- scripts/release_publish.py
+++ scripts/release_publish.py
@@ -40,6 +40,7 @@ def get_file_to_update_list():
     file_list.append(FileWithVersionToUpdate('package.json', r'("version":\s*")([\d\.]+)(")'))
     file_list.append(FileWithVersionToUpdate('sushi-config.yaml', r'(version:\s*")(\d+\.\d+\.\d+)(")'))
     file_list.append(FileWithVersionToUpdate('ruleset.fsh', r'(\*\s*version\s*=\s*")([\d\.]+)(")'))
+    file_list.append(FileWithVersionToUpdate('Einfuehrung.md', r'(Version: \s*)(\d+\.\d+\.\d+)()'))
     return file_list
 
 
