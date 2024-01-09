15e8aca34824f3e6657e8077b9f97f767771d960
Max Theilig <max.theilig@gematik.de>
2024-01-09 16:11:23 +0100

    Revert "Features/diagrams (#37)"
    This reverts commit fdfe8fdb7eb1ea435949d17ec41bddbad9c283e3.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 rm ./Resources/input/fsh/*.fsh
 rm ./Resources/fsh-generated/resources/*
-rm ./Material/docs/*
-rm ./Material/docs/imgsrc/drawio/*
-rm ./Material/docs/imgsrc/plantuml/*
-rm ./ImplementationGuide/diagrams/*
 rm ./README.md
 cat Readme_template.md USAGE.md > ./README.md
 rm Readme_template.md
